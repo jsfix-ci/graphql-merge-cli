3151b6c210c2cf4f96f3f8cf2dc0de2d41502b09
Shreyas Agarkar <shreyas.a24@gmail.com>
2021-01-21 11:55:10 +0530

    corrected the path join process

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ const merge = require("./merge");
 
 module.exports = (path, name) => {
     // Loading all schema files under the received path
-    const files = loadFilesSync(join(__dirname, `${path}/**/*.graphql`));
+    // todo: dirty way of getting the root directory and appending the passed path to it
+    const files = loadFilesSync(join(__dirname, "../", `${path}/**/*.graphql`));
 
-    // Merging all schema files
+    // // Merging all schema files
     const combinedFiles = merge(files);
     
-    // Creating the final combined file
+    // // Creating the final combined file
     fs.writeFileSync(name, combinedFiles);
 
     console.log(`Scanned all schema files under ${path} and created a combined schema file named ${name}`);
