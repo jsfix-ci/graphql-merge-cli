f59adb4f08198c659661fda35984a9be45c85b9f
Shreyas Agarkar <shreyas.a24@gmail.com>
2021-01-27 16:29:25 +0530

    printing the path to be searched

--- src/index.js
+++ src/index.js
@@ -9,13 +9,16 @@ const merge = require("./merge");
 module.exports = (path, name) => {
     // Loading all schema files under the received path
     // todo: dirty way of getting the root directory and appending the passed path to it
-    const files = loadFilesSync(join(__dirname, "../", `${path}/**/*.graphql`));
+    const pathToBeSearched = join(__dirname, "../", `${path}/**/*.graphql`);
 
-    // // Merging all schema files
-    const combinedFiles = merge(files);
+    console.log(`Searching in ${pathToBeSearched}`);
+    const files = loadFilesSync(pathToBeSearched);
+
+    // Merging all schema files
+    const combinedFile = merge(files);
     
-    // // Creating the final combined file
-    fs.writeFileSync(name, combinedFiles);
+    // Creating the final combined file
+    fs.writeFileSync(name, combinedFile);
 
     console.log(`Scanned all schema files under ${path} and created a combined schema file named ${name}`);
 };
